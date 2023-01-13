15a05883f5375eb66f64de1107f1f2a66130a82f
Diego <diegoysole2210@gmail.com>
2023-01-12 23:40:48 -0300

    first commit1

---
+++ js/index.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.navegation', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.central', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-10px'
+  });
+
+  sr.reveal('.tecno', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px'
+  });
+
+
+  sr.reveal('.proyects', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px'
+  });
+
+  sr.reveal('.container_form', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px'
+  });
